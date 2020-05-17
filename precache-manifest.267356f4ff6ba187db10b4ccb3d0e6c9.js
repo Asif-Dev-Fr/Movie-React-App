300333094866415e897e8715958b8ad6c2fd2065
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-05-17 11:24:21 +0200

    Updates

---
+++ precache-manifest.267356f4ff6ba187db10b4ccb3d0e6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8aa9d56de1e9ac00a8670d629eb308",
+    "url": "/Movie-React-App/index.html"
+  },
+  {
+    "revision": "396367e1620ca4ae1b9b",
+    "url": "/Movie-React-App/static/css/main.1515a1e0.chunk.css"
+  },
+  {
+    "revision": "b85318fb0e8c773f47b2",
+    "url": "/Movie-React-App/static/js/2.2639fab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-React-App/static/js/2.2639fab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "396367e1620ca4ae1b9b",
+    "url": "/Movie-React-App/static/js/main.c1d7ecb8.chunk.js"
+  },
+  {
+    "revision": "b1550a3b71532553bdd6",
+    "url": "/Movie-React-App/static/js/runtime-main.06e2ae14.js"
+  }
+]);
